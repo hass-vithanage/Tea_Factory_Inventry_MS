e93ba9aa8f4acebe0c099cd9585d212a496abd88
Hasanga Vithanage <hasangav02@gmail.com>
2026-07-11 21:49:26 +0530

    abstract methods

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,21 @@ abstract class InventoryItem {
     private String itemName;
     private double quantityKg;
 
+    //constructor
     public InventoryItem(String itemId, String itemName, double quantityKg) {
         this.itemId = itemId;
         this.itemName = itemName;
         this.quantityKg = quantityKg;
     }
 
+    // Getter methods
     public String getItemId() { return itemId; }
     public String getItemName() { return itemName; }
     public double getQuantityKg() { return quantityKg; }
+
+    // Abstract methods
+    public abstract void displayStorageInstructions();
+    public abstract double calculateItemValue();
 }
 
 
