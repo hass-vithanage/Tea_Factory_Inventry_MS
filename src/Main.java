1d9acc1001888c44b0ece5775c183ae2b6b02fe9
Hasanga Vithanage <hasangav02@gmail.com>
2026-07-11 21:42:57 +0530

    constructor_01

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ abstract class InventoryItem {
     private String itemId;
     private String itemName;
     private double quantityKg;
+
+    public InventoryItem(String itemId, String itemName, double quantityKg) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.quantityKg = quantityKg;
+    }
 }
 
 
